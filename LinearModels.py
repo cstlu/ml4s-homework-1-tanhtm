1a4526ee7d6e48f3fbedd0b3116a4c1e5131337b
tanhlefthander <tanh.lefthander@gmail.com>
2019-01-31 22:15:00 +0700

    Update Code

--- LinearModels.py
+++ LinearModels.py
@@ -1,16 +1,12 @@
 import numpy as np
 import pandas as pd
 def lm(Y,X):
-	print(X)
-	# Y = np.array[y1,y2..,yn] : yi : value
-	# X = np.array[X1,X2..,Xn] : Xi : vector row
 	Xbar = np.append([np.ones(len(Y))], X, axis = 0)
-	# theta = ((XX^T)^t)XY
 	A = np.dot(Xbar,Xbar.T) 
 	theta = np.dot(np.linalg.pinv(A),np.dot(Xbar,Y))
 	return theta
 
-data = pd.read_csv("Data/NhuCauXeBus.csv")
+data = pd.read_csv("NhuCauXeBus.csv")
 Y = data['Y'].values
 X2 = data['X2'].values
 X3 = data['X3'].values
